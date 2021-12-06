10346a2225e82fbb17e477cdeb13d2da9bfb4398
nayanjha16 <55428109+nayanjha16@users.noreply.github.com>
2021-12-06 19:31:25 +0530

    committing embedding and temperature block

--- N3Net/n3block.py
+++ N3Net/n3block.py
@@ -1,3 +1,4 @@
+#dev: Nayan
 # pylint: skip-file
 # N3Block
 """
@@ -13,5 +14,31 @@ from tensorflow.keras import Model
 class N3Block(Model):
     def __init__(self,  *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+    def embedding_block(self, inp_s):
+        input_shape=inp_s
+        model = Sequential()
+        model.add(Conv2D(64, kernel_size=(3,3),padding="same", input_shape=input_shape))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Conv2D(64, kernel_size=(3,3),padding="same", input_shape=input_shape))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Conv2D(8, kernel_size=(3,3),padding="same", input_shape=input_shape))
+        return model
+
+
+    def temperature_parameter(self, inp_s):
+        input_shape=inp_s
+        model2 = Sequential()
+        model2.add(Conv2D(64, kernel_size=(3,3),padding="same", input_shape=input_shape))
+        model2.add(BatchNormalization())
+        model2.add(Activation('relu'))
+        model2.add(Conv2D(64, kernel_size=(3,3),padding="same", input_shape=input_shape))
+        model2.add(BatchNormalization())
+        model2.add(Activation('relu'))
+        model2.add(Conv2D(3, kernel_size=(3,3),padding="same", input_shape=input_shape))
+        return model2
 
 # %%
